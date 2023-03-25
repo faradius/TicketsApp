f370b8ed5cdff6a4cb010e4d6b324c22f0952ed1
Alejandro <faradius.magic187@gmail.com>
2023-03-25 14:18:23 -0600

    Initial commit

---
+++ app/src/main/java/com/developerscracks/ticketsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.developerscracks.ticketsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
