20fdb44acdc815ca66d0a7c8484b25f626af5b5c
Alejandro <faradius.magic187@gmail.com>
2023-03-25 20:41:02 -0600

    Presentando la información del detalle de la información

--- app/src/main/java/com/developerscracks/ticketsapp/ui/user_client/home/adapters/TicketListAdapter.kt
+++ app/src/main/java/com/developerscracks/ticketsapp/ui/user_client/home/adapters/TicketListAdapter.kt
@@ -6,10 +6,10 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.developerscracks.ticketsapp.data.network.model.Ticket
-import com.developerscracks.ticketsapp.data.network.model.TicketDemo
 import com.developerscracks.ticketsapp.databinding.TicketItemLayoutBinding
 
-class TicketListAdapter(private val onClick: (TicketDemo) -> Unit) : ListAdapter<TicketDemo, TicketListAdapter.TicketsViewHolder>(TicketDiffUtil) {
+class TicketListAdapter(private val onClick: (Ticket) -> Unit) :
+    ListAdapter<Ticket, TicketListAdapter.TicketsViewHolder>(TicketDiffUtil) {
 
     //private var onItemClickListener: ((Ticket) -> Unit)? = null
 
@@ -26,33 +26,41 @@ class TicketListAdapter(private val onClick: (TicketDemo) -> Unit) : ListAdapter
 
     inner class TicketsViewHolder(
         itemBinding: TicketItemLayoutBinding
-    ): RecyclerView.ViewHolder(itemBinding.root){
+    ) : RecyclerView.ViewHolder(itemBinding.root) {
 
         private val numTicket = itemBinding.tvNumTicket
         private val titleTicket = itemBinding.tvTitleTicket
         private val priorityTicket = itemBinding.tvPriorityTicket
         private val severityTicket = itemBinding.tvSeverityIncident
 
-        private var currentTicket: TicketDemo? = null
+        private var currentTicket: Ticket? = null
 
-        fun bind(ticket: TicketDemo){
+        init {
+            itemView.setOnClickListener {
+                currentTicket?.let {
+                    onClick(it)
+                }
+            }
+        }
+
+        fun bind(ticket: Ticket) {
             currentTicket = ticket
 
-            numTicket.text = ticket.numero.toString()
-            titleTicket.text = ticket.titulo
-            priorityTicket.text = ticket.tipoIncidencia
-            severityTicket.text = ticket.gravedad
+            numTicket.text = ticket.numTicket.toString()
+            titleTicket.text = ticket.titleTicket
+            priorityTicket.text = ticket.incidentType
+            severityTicket.text = ticket.severityIncident
         }
     }
 
 }
 
-object TicketDiffUtil : DiffUtil.ItemCallback<TicketDemo>(){
-    override fun areItemsTheSame(oldItem: TicketDemo, newItem: TicketDemo): Boolean {
-        return oldItem.numero == newItem.numero
+object TicketDiffUtil : DiffUtil.ItemCallback<Ticket>() {
+    override fun areItemsTheSame(oldItem: Ticket, newItem: Ticket): Boolean {
+        return oldItem.idTicket == newItem.idTicket
     }
 
-    override fun areContentsTheSame(oldItem: TicketDemo, newItem: TicketDemo): Boolean {
+    override fun areContentsTheSame(oldItem: Ticket, newItem: Ticket): Boolean {
         return oldItem == newItem
     }
 
--- app/src/main/java/com/developerscracks/ticketsapp/ui/user_client/home/fragments/ListTicketsFragment.kt
+++ app/src/main/java/com/developerscracks/ticketsapp/ui/user_client/home/fragments/ListTicketsFragment.kt
@@ -1,15 +1,22 @@
 package com.developerscracks.ticketsapp.ui.user_client.home.fragments
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.developerscracks.ticketsapp.R
+import com.developerscracks.ticketsapp.data.network.model.StatusTicket
+import com.developerscracks.ticketsapp.data.network.model.Ticket
 import com.developerscracks.ticketsapp.data.network.model.TicketDemo
 import com.developerscracks.ticketsapp.databinding.FragmentListTicketsBinding
 import com.developerscracks.ticketsapp.ui.user_client.home.adapters.TicketListAdapter
+import java.time.LocalDate
+import java.util.Date
 
 
 class ListTicketsFragment : Fragment() {
@@ -25,19 +32,65 @@ class ListTicketsFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val tickets = listOf(
-            TicketDemo(1, "Error al iniciar sesión", "bug", "high"),
-            TicketDemo(2, "Mejorar la velocidad de carga", "feature", "medium"),
-            TicketDemo(3, "Problemas con la visualización en móviles", "bug", "medium"),
-            TicketDemo(4, "Agregar opción de cambiar contraseña", "feature", "low")
+            Ticket(
+                "TCK-001",
+                1,
+                "Error al iniciar sesión",
+                "15/01/2022",
+                StatusTicket.NEW,
+                "Juan Pérez",
+                "Soporte",
+                "Bug",
+                "High",
+                "v1.0",
+                "Cuando un usuario intenta iniciar sesión, aparece un mensaje de error indicando que las credenciales son incorrectas.",
+                "",
+                "",
+                ""
+            ),
+            Ticket(
+                "TCK-002",
+                2,
+                "Agregar función de búsqueda",
+                "15/01/2022",
+                StatusTicket.NEW,
+                "María González",
+                "Desarrollo",
+                "Feature",
+                "Medium",
+                "v2.1",
+                "Se requiere agregar una función de búsqueda en la página principal de la aplicación.",
+                "",
+                "",
+                ""
+            ),
+            Ticket(
+                "TCK-003",
+                3,
+                "Problemas con la carga de archivos",
+                "15/01/2022",
+                StatusTicket.NEW,
+                "Pedro Rodríguez",
+                "Atención a clientes",
+                "Bug",
+                "Low",
+                "v1.2",
+                "Algunos usuarios reportan que no pueden cargar archivos en la plataforma.",
+                "",
+                "",
+                ""
+            )
         )
 
         val rvTicketAdapter = TicketListAdapter(
-            onClick = {
-
+            onClick = {selectTicket ->
+                val action = ListTicketsFragmentDirections.actionListTicketsFragmentToDetailTicketFragment(selectTicket)
+                findNavController().navigate(action)
             }
         )
 
